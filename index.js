9b4d1039899c6b2933ff83a35a55da1e16d68e7b
Keval Rakholiya <keval63674@gmail.com>
2022-01-20 21:56:53 +0530

    formate

--- index.js
+++ index.js
@@ -2,57 +2,56 @@ const https = require('https');
 
 function m416(address = '', method = 'GET', data = {}, headers = {}) {
     return new Promise((resolve) => {
-        try{
-        let url = new URL(address);
+        try {
+            let url = new URL(address);
+            if (!Object.keys(headers).length) {
+                headers = {
+                    'Content-Type': 'application/json'
+                }
+            }
 
-    if (!Object.keys(headers).length) {
-        headers = {
-            'Content-Type': 'application/json'
-        }
-    }
+            let options = {
+                hostname: url.hostname,
+                port: url.port,
+                path: url.pathname,
+                method: method,
+                headers
+            }
 
-    let options = {
-        hostname: url.hostname,
-        port: url.port,
-        path: url.pathname,
-        method: method,
-        headers
-    }
+            if (['GET', 'DELETE'].includes(method) && Object.keys(data).length) {
+                options.path += '?' + JSON.stringify(data);
+            }
 
-    if (['GET', 'DELETE'].includes(method) && Object.keys(data).length) {
-        options.path += '?' + JSON.stringify(data);
-    }
+            const req = https.request(options, res => {
+                let status = res.statusCode, message = res.message, tempBuffer = [];
+                res.on('data', chunk => {
+                    tempBuffer.push(chunk)
+                })
+                res.on('end', function () {
+                    tempBuffer = Buffer.concat(tempBuffer).toString();
+                    resolve({
+                        status,
+                        data: tempBuffer,
+                        message
+                    });
+                })
+            })
 
-    const req = https.request(options, res => {
-        let status = res.statusCode, message = res.message, tempBuffer = [];
-        res.on('data', chunk => {
-            tempBuffer.push(chunk)
-        })
-        res.on('end', function () {
-            tempBuffer = Buffer.concat(tempBuffer).toString();
-            resolve({
-                status,
-                data: tempBuffer,
-                message
-            });
-        })
-    })
+            req.on('error', error => {
+                resolve({
+                    status: 512,
+                    message: error.message,
+                    data: error.stack
+                });
+            })
 
-    req.on('error', error => {
-        resolve({
-            status: 512,
-            message: error.message,
-            data: error.stack
-        });
-    })
+            if (['POST', 'PUT'].includes(method) && Object.keys(data).length) {
+                req.write(JSON.stringify(data))
+            }
 
-    if (['POST', 'PUT'].includes(method) && Object.keys(data).length) {
-        req.write(JSON.stringify(data))
-    }
+            req.end();
 
-    req.end();  
-            
-        }catch(e){
+        } catch (e) {
             resolve(e)
         }
     })
@@ -100,3 +99,4 @@ function objIntoParams(obj, prefix) {
 }
 
 module.exports = m416;
+
